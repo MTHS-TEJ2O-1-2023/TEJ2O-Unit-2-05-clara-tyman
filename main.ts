14f3034068c17599c4068dd22fd035b58e13871d
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-09-26 12:54:10 -0400

    ts version

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Clara Tyman
+ * Created on: Sep 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+// varible
+let temperature: number
+
+input.onButtonPressed(Button.A, function () {
+//input
+  temperature = input.temperature()
+
+//process
+  temperature = temperature + 273.15
+  temperature = Math.round(temperature)
+
+//output
+  basic.showString('The temperature is' + temperature.toString() + 'k.')
+
+})
